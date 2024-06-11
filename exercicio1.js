c6fc8cc8a3ea1be8bb70ce6278df21f3f9981eed
beatrizoliveir <147163128+beatrizoliveir@users.noreply.github.com>
2024-06-11 09:57:56 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,5 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+numeros.pop()
+
+console.log(numeros);
+
---
+++ exercicio2.js
@@ -0,0 +1,6 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+
+const tirarelemento = numeros.slice(1)
+
+console.log(tirarelemento);
+
