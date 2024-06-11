cffbaf1bca824bca8bd2cc0c0bce5b59f092f13d
beatrizoliveir <147163128+beatrizoliveir@users.noreply.github.com>
2024-06-11 11:00:11 -0300

    Add files via upload

---
+++ 3-adicionar.js
@@ -0,0 +1,4 @@
+const numeros = [1, 2, 3, 4, 5, 6]
+numeros.unshift(0)
+
+console.log(numeros);
---
+++ 5-modificar.js
@@ -0,0 +1,4 @@
+const numeros = [1, 2, 3, 4, 5, 6];
+numeros.splice(2,2,10);
+
+console.log(numeros);
